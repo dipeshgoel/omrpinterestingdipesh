579a284f76682ba13f7926c892684c409c9311b6
Dipesh Goel <dipeshgoel@gmail.com>
2014-07-08 07:29:18 -0700

    Updated Paperclip settings for Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,9 +90,10 @@ Rails.application.configure do
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :url =>':s3_domain_url'
-    }
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename', 
   }
 
 end
