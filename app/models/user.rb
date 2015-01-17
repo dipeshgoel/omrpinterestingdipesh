7b523556630b158c2d1d2019b6bb203aee62f384
Dipesh Goel <dipeshgoel@gmail.com>
2015-01-17 09:20:46 -0800

    Initial Soundfuzed Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins, dependent: :distroy
+  has_many :pins, dependent: :destroy
 
 	validates :name, presence: true
 
